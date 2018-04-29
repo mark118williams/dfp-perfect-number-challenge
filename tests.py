ca0919eb59a633ce8a1c0b2bae4dfe2b01a2cb32
Mark Williams <mark118williams@gmail.com>
2018-04-29 22:03:31 +0100

    Update tests commentary

--- tests.py
+++ tests.py
@@ -3,7 +3,9 @@ from perfect_numbers import classify
 def function_test():
 	# Ensure 6 is classified as a perfect numbe 
     assert classify(6) == 'perfect'
+    # Ensure 12 is classified as an abundant number
     assert classify(12) == 'abundant'
+    # Ensure 8 in classified as a deficient number
     assert classify(8) == 'deficient'
 
 function_test()
